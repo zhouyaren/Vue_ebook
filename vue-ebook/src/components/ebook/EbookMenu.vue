01fed1033e14fbef924e14c9a0eb57c23c27334f
zhouyaren <1601704037@qq.com>
2020-03-22 11:07:39 +0800

    目录蒙板滑动效果

--- vue-ebook/src/components/ebook/EbookMenu.vue
+++ vue-ebook/src/components/ebook/EbookMenu.vue
@@ -20,6 +20,7 @@
       <ebook-setting-font-popup></ebook-setting-font-popup>
       <ebook-setting-theme></ebook-setting-theme>
       <ebook-setting-progress></ebook-setting-progress>
+      <ebook-slide></ebook-slide>
     </div>
 </template>
 
@@ -29,6 +30,7 @@ import EbookSettingFont from './EbookSettingFont'
 import EbookSettingFontPopup from './EbookSettingFontPopup'
 import EbookSettingTheme from './EbookSettingTheme'
 import EbookSettingProgress from './EbookSettingProgress'
+import EbookSlide from './EbookSlide'
 export default {
   mixins: [ebookMixin],
   methods: {
@@ -40,7 +42,8 @@ export default {
     EbookSettingFont,
     EbookSettingFontPopup,
     EbookSettingTheme,
-    EbookSettingProgress
+    EbookSettingProgress,
+    EbookSlide
   }
 }
 </script>
---
+++ vue-ebook/src/components/ebook/EbookSlide.vue
@@ -0,0 +1,48 @@
+<template>
+  <transition name="fade-slide-right">
+    <div class="slide-content-wrapper" v-show="menuVisible && settingVisible === 3">
+      <div class="content">
+        <div class="content-page-wrapper">
+          <div class="content-page"></div>
+          <div class="content-page-tab"></div>
+        </div>
+      </div>
+      <div class="content-bg"></div>
+    </div>
+  </transition>
+</template>
+
+<script>
+import { ebookMixin } from '../../utils/mixin'
+export default {
+  mixins: [ebookMixin],
+  name: 'EbookSlide'
+}
+</script>
+
+<style lang="scss" rel="stylesheet/scss" scoped>
+@import "../../assets/styles/global";
+  .slide-content-wrapper{
+    position: absolute;
+    top: 0;
+    left: 0;
+    z-index: 300;
+    width: 100%;
+    height: 100%;
+    display: flex;
+    .content{
+      flex: 0 0 85%;
+      width: 85%;
+      height: 100%;
+      .content-page-wrapper{
+        .content-page{}
+        .content-page-tab{}
+      }
+    }
+    .content-bg{
+      flex: 0 0 15%;
+      width: 15%;
+      height: 100%;
+    }
+  }
+</style>
