6ed3fd3b5cef945676ca5be2bae728d96b4990c5
zhouyaren <1601704037@qq.com>
2020-03-29 10:24:45 +0800

    标题搜索框交互细节分析

---
+++ vue-ebook/src/components/home/SearchBar.vue
@@ -0,0 +1,102 @@
+<template>
+  <div class="search-bar">
+    <div class="search-bar-title-wrapper">
+      <div class="title-icon-back-wrapper">
+        <span class="icon-back icon"></span>
+      </div>
+      <div class="title-text-wrapper">
+        <span class="title-text title">{{$t('home.title')}}</span>
+      </div>
+      <div class="title-icon-shake-wrapper">
+        <span class="icon-shake icon"></span>
+      </div>
+    </div>
+    <div class="search-bar-input-wrapper">
+      <div class="search-bar-input">
+        <span class="icon-search icon"></span>
+        <input class="input"
+               type="text"
+               :placeholder="$t('home.hint')"
+                v-model="searchText">
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'SearchBar',
+    data() {
+      return {
+        searchText:''
+      }
+    }
+  }
+</script>
+
+<style lang="scss" rel="stylesheet/scss" scoped>
+  @import "../../assets/styles/global";
+  .search-bar{
+    position: relative;
+    width: 100%;
+    .search-bar-title-wrapper{
+      position: relative;
+      top: 0;
+      left: 0;
+      width: 100%;
+      height: px2rem(42);
+      .title-icon-back-wrapper{
+        position: absolute;
+        left: px2rem(15);
+        top: 0;
+        height: px2rem(42);
+        @include center;
+      }
+      .title-text-wrapper{
+        width: 100%;
+        height: px2rem(42);
+        @include center;
+      }
+      .title-icon-shake-wrapper{
+        position: absolute;
+        right: px2rem(15);
+        top: 0;
+        height: px2rem(42);
+        @include center;
+      }
+    }
+    .search-bar-input-wrapper{
+      width: 100%;
+      height: px2rem(52);
+      padding: px2rem(10);
+      box-sizing: border-box;
+      .search-bar-input{
+        width: 100%;
+        background: #f4f4f4;
+        border-radius: px2rem(20);
+        padding: px2rem(5) px2rem(15);
+        box-sizing: border-box;
+        border: px2rem(1) solid #eee;
+        @include left;
+        .icon-search{
+          color: #999;
+        }
+        .input{
+          width: 100%;
+          height: px2rem(22);
+          border: none;
+          background: transparent;
+          margin-left: px2rem(10);
+          font-size: px2rem(12);
+          color: #666;
+          &:focus {
+            outline: none;
+          }
+          &::-webkit-input-placeholder {
+            color: #cccccc;
+          }
+        }
+      }
+    }
+  }
+</style>
---
+++ vue-ebook/src/views/store/StoreHome.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="storehome">
+      <search-bar></search-bar>
+    </div>
+</template>
+
+<script>
+  import SearchBar from '../../components/home/SearchBar.vue'
+  export default {
+    name: 'StoreHome',
+    components: {
+      SearchBar
+    }
+  }
+</script>
+
+<style lang="scss" rel="stylesheet/scss" scoped>
+@import "../../assets/styles/global";
+
+</style>
